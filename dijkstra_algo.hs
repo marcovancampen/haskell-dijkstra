2deac8428f1275431d252caafbc05c2ba116ba60
Marco van Campen <mjtvancampen@gmail.com>
2026-09-13 09:35:37 +0200

    added new data type, added a function to see the shortest distance. and to see if it exist in the hashmap.

--- dijkstra_algo.hs
+++ dijkstra_algo.hs
@@ -1,21 +1,26 @@
 import Debug.Trace
 import qualified Data.HashMap.Strict as HM
 import Data.HashMap.Strict (HashMap)
+import Data.Heap (MinPrioHeap)
+import qualified Data.HashSet as HS
+import Data.HashSet (HashSet)
+import Data.Hashable (Hashable)
+import Data.Maybe (fromMaybe)
 
 
-graph1 :: Graph
-graph1 = Graph $ HM.fromList
-  [ ("A", [("D", 100), ("B", 1), ("C", 20)])
-  , ("B", [("D", 50)])
-  , ("C", [("D", 20)])
-  , ("D", [])
-  ]
  
 newtype Graph = Graph
     { edges :: HashMap String [(String, Int)] }
 
 data Distance a = Dist a | Infinity --a is een generic. ook zorgt dist a er voor dat deze type of een distance van a krijgt of oneindig
     deriving (Show, Eq) --adding de default functions. dit stukje zorgt er voor dat er boiler plate code komt voor dit data type
+
+data DijkstraState = DijkstraState
+    { 
+        vistitedSet :: HashSet String, -- zorgt ervoor dat we niet terug kunnen komen op nodes waar we al zijn geweest omdat hashsets alleen maar unique elemente toe laat.
+        distanceMap :: HashMap String (Distance Int),   
+        nodeQueue :: MinPrioHeap (Distance Int) String
+    }
 
 instance (Ord a) => Ord (Distance a) where -- dit maakt de regels voor de funtie. dit zorgt ervoor dat de generic wel orderable moet zijn. ORD type class. ik heb dit hand matig geschreven omdat dit me meer controle geeft. als ik dat naemlijk had gedaan met deriving. Als iemand het dan aan past dan zou de order niet meer kloppen op deze manier kan dat niet gebeuren.
     Infinity <= Infinity = True -- Infinity is altijd gelijk aan Infinity 
@@ -27,4 +32,19 @@ addDistance :: (Num a) => Distance a -> Distance a -> Distance a -- forced dat d
 addDistance (Dist x ) (Dist y ) =  Dist (x + y)
 addDistance _ _ = Infinity
 
-        
+
+lookupTable :: (Hashable k , Eq k ) => HashMap k (Distance d) -> k -> Distance d -- forceed dat k hashable is en dat k ook verglijkbaar moet zijn. dit zorgt ervoor dat we een bepaald patroon behouden. returned een distance (custom data type hier boven)
+lookupTable distanceMap key = fromMaybe Infinity(Hm.lookup key distanceMap) -- kijkt in de hashmap of er een instance is van een distance met de key (k) zo ja returned dit dat getal (distance d). zo niet returned het Infinity. dit mag omdat we boven aan de data type hebben gemaakt en dit is 1 van de toegestaande waardes.
+
+
+findShortestDistance :: Graph -> String -> String -> Distance Int -- neemt een de graph, de start node (string) en destiantion node (string) en berekend de distance. de kleinste wordt gereturned in als distance
+
+
+
+graph1 :: Graph
+graph1 = Graph $ HM.fromList
+  [ ("A", [("D", 100), ("B", 1), ("C", 20)])
+  , ("B", [("D", 50)])
+  , ("C", [("D", 20)])
+  , ("D", [])
+  ]
